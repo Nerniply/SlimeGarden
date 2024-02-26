b85b9db4cf30d26fda017e6f17d32b8d7c2d5d9d
luke@zebrowski.net <luke@zebrowski.net>
2024-02-26 11:15:37 -0800

    Slime animations finished

--- rosa.gd
+++ rosa.gd
@@ -4,12 +4,36 @@ class_name Player
 var maxhp = 25
 var curhp = 25
 var spd = 10
+var direction = "X"
 
 func _physics_process(delta):
-	var move = Vector2()
-	move.x = spd*(int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)))
-	move.y = spd*(int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W)))
-	position += move
+	var input = Vector2()
+	if Input.is_key_pressed(KEY_D):
+		input.x = 1
+		$AnimatedSprite2D.play("move_R")
+		direction = "R"
+	if Input.is_key_pressed(KEY_A):
+		input.x = -1
+		$AnimatedSprite2D.play("move_L")
+		direction = "L"
+	if Input.is_key_pressed(KEY_S):
+		input.y = 1
+		if direction == "R":
+			$AnimatedSprite2D.play("move_R")
+		else: $AnimatedSprite2D.play("move_L")
+	if Input.is_key_pressed(KEY_W):
+		input.y = -1
+		if direction == "L":
+			$AnimatedSprite2D.play("move_L")
+		else: $AnimatedSprite2D.play("move_R")
+	if !Input.is_key_pressed(KEY_D) and !Input.is_key_pressed(KEY_A) and !Input.is_key_pressed(KEY_S) and !Input.is_key_pressed(KEY_W):
+		$AnimatedSprite2D.play("idle")
+	position += input.normalized()*spd
+	
+	#var move = Vector2()
+	#move.x = spd*(int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)))
+	#move.y = spd*(int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W)))
+	#position += move
 
 func SetMaxHP(newhp):
 	maxhp = newhp
